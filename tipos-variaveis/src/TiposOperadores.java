5499a2228ce8956ea977cd926f6466951c3d6060
CarolineT9 <caroltvrs@gmail.com>
2023-03-15 10:23:12 -0300

    Exercícios de operadores no java.

---
+++ tipos-variaveis/src/TiposOperadores.java
@@ -0,0 +1,47 @@
+public class TiposOperadores {
+       /*Operadores 
+         * == igual
+         * != diferente
+         * < menor
+         * > maior
+         * <= menor igual
+         * >= maior igual
+         * 
+         * Operador de atribuição é representado pelo simbolo de =  
+         * operador de atribuição é utilizado para definir o valor inicial ou sobrescrever o valor de uma variável
+         * 
+         * Operadores aritméticos: perador aritmético é utilizado 
+         * para realizar operações matemáticas entre valores numéricos, podendo se tornar ou não uma expressão mais complexa.
+         
+
+         
+        
+        public static void main(String[] args) {
+            String nomeCompleto = "LINGUAGEM" + "JAVA";
+            System.out.println(nomeCompleto);
+            }
+            */
+            /**
+             * @param args
+             */
+            public static void main(String[] args) {
+            String concatenacao ="?";
+
+            concatenacao = 1+1+1+"1";
+                System.out.println(concatenacao);
+
+            concatenacao = 1+"1"+1+1;
+                System.out.println(concatenacao);
+
+            concatenacao = 1+"1"+1+"1";
+                System.out.println(concatenacao);
+
+            concatenacao = "1"+1+1+1;
+                System.out.println(concatenacao);
+            
+            concatenacao = "1"+(1+1+1);
+                System.out.println(concatenacao);
+            }
+         
+         
+}
