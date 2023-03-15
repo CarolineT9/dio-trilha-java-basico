7f7bfe39593ef4f5369cf8b59a4f1bc859d69c02
CarolineT9 <caroltvrs@gmail.com>
2023-03-15 10:46:42 -0300

    Exercício de operadores unários

--- tipos-variaveis/src/TiposOperadores.java
+++ tipos-variaveis/src/TiposOperadores.java
@@ -6,12 +6,19 @@ public class TiposOperadores {
          * > maior
          * <= menor igual
          * >= maior igual
-         * 
+         
          * Operador de atribuição é representado pelo simbolo de =  
-         * operador de atribuição é utilizado para definir o valor inicial ou sobrescrever o valor de uma variável
-         * 
+           operador de atribuição é utilizado para definir o valor inicial ou sobrescrever o valor de uma variável
+        
          * Operadores aritméticos: perador aritmético é utilizado 
-         * para realizar operações matemáticas entre valores numéricos, podendo se tornar ou não uma expressão mais complexa.
+           para realizar operações matemáticas entre valores numéricos, podendo se tornar ou não uma expressão mais complexa.
+         
+         * Operadores Unários: realizam trabalhos como incrementar, decrementar, inverter valores numéricos e booleanos.
+         *  (+) Operador unário de valor positivo – números são positivos sem esse operador explicitamente;
+            (-) Operador unário de valor negativo – nega um número ou expressão aritmética;
+            (++) Operador unário de incremento de valor – incrementa o valor em 1 unidade;
+            (--) Operador unário de decremento de valor – decrementa o valor em 1 unidade;
+            (!) Operador unário lógico de negação – nega o valor de uma expressão booleana;
          
 
          
@@ -23,7 +30,7 @@ public class TiposOperadores {
             */
             /**
              * @param args
-             */
+             
             public static void main(String[] args) {
             String concatenacao ="?";
 
@@ -42,6 +49,17 @@ public class TiposOperadores {
             concatenacao = "1"+(1+1+1);
                 System.out.println(concatenacao);
             }
-         
+         */
+
+         public static void main(String[] args) {
+            int numero = 5;
+            numero = - numero;
+            
+            System.out.println(numero);
+
+            numero  =  numero * -1; /* nesse caso para tornar o número positivo é preciso multiplicar por -1 */ 
+
+            System.out.println(numero);
+         }
          
 }
